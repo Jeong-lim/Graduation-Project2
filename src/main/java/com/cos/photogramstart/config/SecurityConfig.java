12243906fceda6f85298e7bc549c336c84f8c2a9
Jeong-lim <wjdfla401@naver.com>
2022-10-23 19:43:10 +0900

    10/23

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화시키는 어노테이션
+@Configuration // IOC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http); // 삭제 - 기존 시큐리티가 가지고 있는 기능이 다 비활성화 됨
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/");
+        // 인증이 필요한 페이지는 아무나 못들어가고
+        // 로그인이 성공되면 / 페이지로 자동으로 이동
+
+    }
+}
