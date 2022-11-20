d1570ccf5855ccd51a802f1253a7216225fed1c1
jeonglim <wjdfla401@naver.com>
2022-11-20 14:13:40 +0900

    메인 이미지뷰

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -82,60 +82,19 @@ function getStoryItem(image) {
 
 // (2) 스토리 스크롤 페이징하기
 $(window).scroll(() => {
-	//console.log("윈도우 scrollTop", $(window).scrollTop());
-	//console.log("문서의 높이", $(document).height());
-	//console.log("윈도우 높이", $(window).height());
-	
-	let checkNum = $(window).scrollTop() - ( $(document).height() - $(window).height() );
-	//console.log(checkNum);
-	
-	if(checkNum < 1 && checkNum > -1){
-		page++;
-		storyLoad();
-	}
+
 });
 
 
 // (3) 좋아요, 안좋아요
 function toggleLike() {
-	let likeIcon = $("#storyLikeIcon-1");
-	if (likeIcon.hasClass("far")) {
-		likeIcon.addClass("fas");
-		likeIcon.addClass("active");
-		likeIcon.removeClass("far");
-	} else {
-		likeIcon.removeClass("fas");
-		likeIcon.removeClass("active");
-		likeIcon.addClass("far");
-	}
+
 }
 
 // (4) 댓글쓰기
 function addComment() {
 
-	let commentInput = $("#storyCommentInput-1");
-	let commentList = $("#storyCommentList-1");
 
-	let data = {
-		content: commentInput.val()
-	}
-
-	if (data.content === "") {
-		alert("댓글을 작성해주세요!");
-		return;
-	}
-
-	let content = `
-			  <div class="sl__item__contents__comment" id="storyCommentItem-2""> 
-			    <p>
-			      <b>GilDong :</b>
-			      댓글 샘플입니다.
-			    </p>
-			    <button><i class="fas fa-times"></i></button>
-			  </div>
-	`;
-	commentList.prepend(content);
-	commentInput.val("");
 }
 
 // (5) 댓글 삭제
